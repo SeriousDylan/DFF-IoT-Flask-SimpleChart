62c0d17a39496234d84321cd30cc46f4667abb98
Dylan <dylan.f.findlay@gmail.com>
2020-10-13 16:03:22 +0800

    fix typo

--- static/js/line-chart.js
+++ static/js/line-chart.js
@@ -1,35 +1,32 @@
 var lineChartCanvas1 = document.getElementById('line-chart')
 
 var lineData = {
-	labels: [
-		'15-34', '35-44', '45-54', '55-64', '65+',
-	],
-		datasets: [
-		{
-			label: 'Female',
-			line: '',
-			data: [158.6, 140, 146, 81.2, 24.1,],
-			borderWidth: 2,
-			lineTension: 0.2,
-			fill: false,
-			borderColor: [
-				'rgba(128, 64, 65, 1)',
-			],
-		},
-	
-		{
-			lable: 'Male',
-			line: '',
-			data: [212.6, 254.4, 261.9, 182.8, 67],
-			borderWidth: 2,
-			lineTension: 0.2,
-			fill: false,
-			borderColor: [
-				'rgba(64, 64, 128, 1)',
-			],
-		},
-	],
-},
+	labels: ['15-34', '35-44', '45-54', '55-64', '65+',],
+		
+
+	datasets: [
+	{
+		label: 'Female',
+		line: '',
+		data: [158.6, 140, 146, 81.2, 24.1,],
+		borderWidth: 2,
+		lineTension: 0.2,
+		fill: false,
+		borderColor: [
+			'rgba(128, 64, 65, 1)',
+		],
+	}, {
+		label: 'Male',
+		line: '',
+		data: [212.6, 254.4, 261.9, 182.8, 67],
+		borderWidth: 2,
+		lineTension: 0.2,
+		fill: false,
+		borderColor: [
+			'rgba(64, 64, 128, 1)',
+		],
+	}]
+}
 
 var lineOptions = {
 	legend: {display: false},
